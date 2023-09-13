4244ff897794943bbde671f84df87353f7d87f2c
alessandropasla <alessandropasla026@student.unsrat.ac.id>
2023-09-14 00:50:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# metric_converter-Alessandro_Pasla-IONIC
+
+![ezgif com-video-to-gif](https://github.com/alessandropasla/metric_converter-Alessandro_Pasla-IONIC/assets/132435524/82bc4f8f-756a-4eb0-b8b0-aa191ab52dbc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
